1a015e43e7cfdf0a2f609ff8aeeb49665489a0da
Rotich <jane.rotich@student.moringaschool.com>
2024-05-12 15:34:26 +0300

    feat:update

--- app.py
+++ app.py
@@ -3,10 +3,29 @@ from flask_migrate import Migrate
 from models.dbconfig import db
 from models.user import User
 from models.passwordresettoken import PasswordResetToken
+import cloudinary
+import cloudinary.uploader
+import jwt
+import os
+import base64
+from werkzeug.security import generate_password_hash, check_password_hash
+from flasgger import Swagger
+from datetime import datetime, timedelta
+from utils import cloudconfig
+
+# swagger -create documentation for our app. uses comment block
+
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SWAGGER'] = {
+    'title': 'Auth and Cloudinary Api Docs',
+    'universion': 3
+}
 
+swagger = Swagger(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+# binding sqlalchemy instance
 db.init_app(app)
 
 migrate = Migrate(app, db)
--- models/utils.py
+++ models/utils.py
@@ -2,5 +2,5 @@ import cloudinary
 cloudconfig = cloudinary.config(
     cloud_name = 'ds9vevni8',
     api_key = '193594167848786',
-    api_secret = 'JjK5divONfAY7HXnD6AofrQ4yR8'
+    api_secret = ''
 )
